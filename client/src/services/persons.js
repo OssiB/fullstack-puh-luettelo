687b5264f051ff4aeec15c63db34318080e42604
Ossi Bister <ossi.bister@gmail.com>
2025-04-06 20:05:01 +0300

    config base added

--- client/src/services/persons.js
+++ client/src/services/persons.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/api/persons'
+
+const baseUrl = `${import.meta.env.VITE_API_BASE_URL}/persons`
 
 const getAll = () => {
   const request = axios.get(baseUrl)
